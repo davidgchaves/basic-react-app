e260b34d7c331dd9b5a107b718798a196599d6d7
David G Chaves <davidgchaves@gmail.com>
2022-11-28 18:32:52 +0100

    Switch to from HashRouter to BrowserRouter

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./login";
 import { ListPage } from "./list";
 import { DetailPage } from "./detail";
@@ -7,13 +7,13 @@ import { DetailPage } from "./detail";
 export const App = () => {
   return (
     <>
-      <HashRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/list" element={<ListPage />} />
           <Route path="/detail" element={<DetailPage />} />
         </Routes>
-      </HashRouter>
+      </Router>
     </>
   );
 };
