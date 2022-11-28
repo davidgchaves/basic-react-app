69a5472f3ddebb847667eb978a0fe5f4c4708441
David G Chaves <davidgchaves@gmail.com>
2022-11-28 20:33:12 +0100

    List component ready

--- src/list.tsx
+++ src/list.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+interface MemberEntity {
+  id: string;
+  login: string;
+  avatar_url: string;
+}
+
 export const ListPage: React.FC = () => {
+  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+
+  React.useEffect(() => {
+    fetch("https://api.github.com/orgs/facebook/members")
+      .then((r) => r.json())
+      .then(setMembers);
+  }, []);
+
   return (
     <>
       <h3>Ola dende List</h3>
-      <Link to="/detail">Navigate to Detail page</Link>
+
+      <div className="user-list-container">
+        <span className="header">Avatar</span>
+        <span className="header">Id</span>
+        <span className="header">Name</span>
+        {members.map((member) => (
+          <React.Fragment key={member.id}>
+            <img src={member.avatar_url} />
+            <span>{member.id}</span>
+            <Link to={`details/${member.login}`}>{member.login}</Link>
+          </React.Fragment>
+        ))}
+      </div>
     </>
   );
 };
