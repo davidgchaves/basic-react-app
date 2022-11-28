f2a90a2c191e96112615b88cd5025ff4a41560eb
David G Chaves <davidgchaves@gmail.com>
2022-11-28 19:02:15 +0100

    initial solution for Login exercise

--- src/login.tsx
+++ src/login.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export const LoginPage: React.FC = () => {
   const navigate = useNavigate();
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isValidCredentials = (username: string, password: string) =>
+    username === "admin" && password === "test";
+
   const handleNavigate = () => {
-    navigate("/list");
+    isValidCredentials(username, password)
+      ? navigate("/list")
+      : alert("Wrong credentials");
   };
 
   return (
     <>
       <h3>Ola dende Login</h3>
+      <div>
+        <div>
+          <label htmlFor="">Username: </label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <label htmlFor="">Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
       <button onClick={handleNavigate}>Login</button>
     </>
   );
