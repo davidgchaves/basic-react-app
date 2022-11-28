00b0087511d2263c44c8e005959423603c6a6152
David G Chaves <davidgchaves@gmail.com>
2022-11-28 17:47:11 +0100

    Starting point for App component

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { MemberEntity } from './model';
-
-import { MemberTableRow } from './member-table-row';
-import { MemberTableHeader } from "./member-table-header";
 
 export const App = () => {
-  const [members, setMembers] = React.useState<MemberEntity[]>([]);
-
-  React.useEffect(() => {
-    fetch('https://api.github.com/orgs/lemoncode/members')
-      .then(r => {
-        if (r.ok) { return r.json(); }
-      })
-      .then(json => { setMembers(json) })
-      .catch(console.error);
-  }, []);
-
   return (
-    <div className="user-list-container">
-      <MemberTableHeader />
-      {members.map((member) => (
-        <MemberTableRow key={member.id} member={member} />
-      ))}
-    </div>
+    <>
+      <div>Ola dende App</div>
+    </>
   );
 };
