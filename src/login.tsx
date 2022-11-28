0f47c31221cab72b4ede1ad013ceee1b736c0565
David G Chaves <davidgchaves@gmail.com>
2022-11-28 18:30:32 +0100

    Add login button

--- src/login.tsx
+++ src/login.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/list");
+  };
+
   return (
     <>
       <h3>Ola dende Login</h3>
-      <Link to="/list">Navigate to List page</Link>
+      <button onClick={handleNavigate}>Login</button>
     </>
   );
 };
