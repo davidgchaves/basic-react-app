de7c8f9bbe597ff16c942974a216d11556f63088
David G Chaves <davidgchaves@gmail.com>
2022-11-29 18:34:21 +0100

    Into the Router Rabbit Hole

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { LoginPage } from "./scenes/login";
-// import { ListPage } from "./scenes/list";
-// import { DetailPage } from "./scenes/detail";
-// import { LoginPage } from "@/scenes/login";
-// import { ListPage } from "@/scenes/list";
-// import { DetailPage } from "@/scenes/detail";
-import { DetailPage, LoginPage, ListPage } from "@/scenes";
+import { RouterComponent } from "./core/router/router.component";
 
 export const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/detail/:id" element={<DetailPage />} />
-        </Routes>
-      </Router>
-    </>
-  );
+  return <RouterComponent />;
 };
